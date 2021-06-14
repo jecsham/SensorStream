ad3e9708bbf87cbd3cf31f4d716c90bf0e76a041
jecsham <jecsham27@gmail.com>
2021-06-14 16:12:30 -0500

    Implemented mutex for singleton

--- SensorsStream/Program.cs
+++ SensorsStream/Program.cs
@@ -1,13 +1,25 @@
 ﻿using System;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace SensorStream
 {
     static class Program
     {
+        private static Mutex mutex = null;
         [STAThread]
         static void Main()
         {
+            const string appName = "com.jecsham.sensorstream";
+            bool createdNew;
+
+            mutex = new Mutex(true, appName, out createdNew);
+            if (!createdNew)
+            {
+                MessageBox.Show("There is already an instance of Sensor Stream running", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
